81da4eb2043c9c8d840a65123cd24eb536166090
MattHeffNT <heffa2004@gmail.com>
2022-08-30 18:06:07 +1000

    mapped emojis into container,cleaned up comments

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,7 +1,8 @@
 import './ExploreContainer.css';
 import emojis from '../assets/emojis.json'
+// import '../assets/images'
 import { useState, useEffect, useRef } from 'react';
-import { IonContent, IonHeader, IonSearchbar, IonFooter, IonGrid, IonRow, IonCol } from '@ionic/react';
+import { IonContent, IonSearchbar, IonImg, IonGrid, IonRow, IonCol } from '@ionic/react';
 
 // At some stage will need to stop the name prop being passed to
 // each page
@@ -13,16 +14,16 @@ interface ContainerProps {
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
 
   const [emojisData, setEmojisData] = useState([]);
+  const [searchText, setSearchText] = useState('');
 
 
   // mount the icons container to try and load in the images in the emoji folder
-  const iconsContainer = useRef<HTMLDivElement>(null); const [searchText, setSearchText] = useState('');
+  const iconsContainer = useRef<HTMLDivElement>(null);
 
+  // might be better to use "useRef" for this 
   useEffect(() => {
 
-    // console.log(emojis[1])
     var divElement = iconsContainer.current;
-    // console.log(divElement);
 
     // divElement.innerHTML += `<img src="../assets/emojis/ure.png"></img>`
     // append images here might have to chuck in the other fetch data in here too then just put it all togher
@@ -30,25 +31,45 @@ const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
 
   }, []);
 
-  // call fetch once image clicked and populate modal with respective data
+
+
+  // get array of emoji json object keys before using map method
+  const result: any = Object.keys(emojis).map((key) => {
+    return { [key]: emojis[key as keyof typeof emojis] };
+  });
+
+  var obj: any = emojis
+  var arr: any = obj.emojis
+  console.log(arr)
 
   return (
+
     < IonContent >
-
-
       {/* hmm might pull this out to its own component, keeping here for the moment so that it will be easier to search */}
       {/* the text from the json  */}
 
-      {/* insert the emojis into their own column */}
       <div className='container'>
+
+
+        {/*  i think i definitely need to have search bar as seperate component so that it doesn't update state */}
+        {/* of the container */}
+
         <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)}></IonSearchbar>
         <IonGrid>
           <IonRow>
-            <IonCol>
 
-              col1
+            {/* map emoji images here, then on click load modal with larger emoji, translation options, audio, and share button*/}
+            {arr.map((emoji: any) => (
 
-            </IonCol>
+              <IonCol>
+                <IonImg src={emoji.file} />
+              </IonCol>
+
+            ))}
+
+            {/* keep this here so that emojis on the bottom row are gridded correctly */}
+            <IonCol></IonCol>
+
           </IonRow>
         </IonGrid>
       </div>
