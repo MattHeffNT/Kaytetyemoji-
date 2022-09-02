2cb39534f21fdd5b78f911a118dafb3e4cb5ddd5
MattHeffNT <heffa2004@gmail.com>
2022-09-02 15:03:15 +1000

    passing json data into modal, added some styling

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,9 +1,11 @@
 import './ExploreContainer.css';
 import emojis from '../assets/emojis.json'
 // import '../assets/images'
-import { useState } from 'react';
-import { IonContent, IonSearchbar, IonHeader, IonToolbar, IonTitle, IonImg, IonGrid, IonRow, IonButtons, IonButton, IonCol, IonModal } from '@ionic/react';
+import { useEffect, useState } from 'react';
 
+// import Ion components (seperated into two lines for readability)
+import { IonPage, IonContent, IonSearchbar, IonHeader, IonToolbar, IonTitle, IonImg, IonGrid, IonRow, IonCol } from '@ionic/react';
+import { IonModal, IonChip, IonButtons, IonButton } from '@ionic/react';
 // At some stage will need to stop the name prop being passed to
 // each page
 
@@ -11,47 +13,22 @@ interface ContainerProps {
   name: string;
 }
 
-var localArr: any = []
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
 
   const [emojisData, setEmojisData] = useState([]);
   const [searchText, setSearchText] = useState('');
-  //modal state
-  const [isOpen, setIsOpen] = useState(false);
+
+  const [myModal, setMyModal] = useState({ isOpen: false })
 
   // get array of emoji json object keys before using map method
   const result: any = Object.keys(emojis).map((key) => {
     return { [key]: emojis[key as keyof typeof emojis] };
   });
 
+
+  // variables to map in the component
   var obj: any = emojis
   var arr: any = obj.emojis
-
-
-
-  // write function to open modal here...maybe?
-  const modal = (emojiObject: any) => {
-
-    console.log(emojiObject);
-    setIsOpen(true)
-
-    localArr.push(emojiObject)
-
-    return emojiObject
-
-    //option 1:
-    // pass value/object into array then just pull it out in the modal JSX? (will be agnostic to device constraints)
-
-    // option 2:
-    // one option here is to pass in variable into session storage then just retrieve in the modal jsx? not sure if this will
-    // work in IOS/Android once built
-
-    // option 3:
-    // map the json again in the modal jsx
-
-  }
-
-
 
   return (
 
@@ -61,20 +38,17 @@ const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
 
       <div className='container'>
 
-        <IonSearchbar id="search" value={searchText} onIonChange={e => setSearchText(e.detail.value!)} showClearButton="always" ></IonSearchbar>
-
-
         {/*  i think i definitely need to have search bar as seperate component so that it doesn't update state */}
         {/* of the container.... also maybe put into the toolbar so that it's fixed when user scrolls down*/}
-
+        <IonSearchbar id="search" value={searchText} onIonChange={e => setSearchText(e.detail.value!)} showClearButton="always" ></IonSearchbar>
 
         <IonGrid>
           <IonRow>
 
-            {/* map emoji images here, then on click load modal with larger emoji, translation options, audio, and share button*/}
+            {/* map emoji images here, then on click open and load modal with larger emoji, translation options, audio, and share button*/}
             {arr.map((emoji: any) => (
               <IonCol>
-                <IonImg src={emoji.file} onClick={() => modal(emoji)} />
+                <IonImg src={emoji.file} onClick={() => { setMyModal({ isOpen: true }); setEmojisData(emoji); console.log(emoji) }} />
               </IonCol>
             ))}
 
@@ -85,30 +59,51 @@ const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
         </IonGrid>
       </div>
 
+      <MyModal
+        isOpen={myModal.isOpen}
+        onClose={() => setMyModal({ isOpen: false })}
+        initialData={emojisData}
+      />
 
-      {/* Modal  */}
-      <IonModal isOpen={isOpen} >
-        <IonHeader>
-          <IonToolbar>
-            {/* <IonTitle>Modal</IonTitle> */}
-            <IonButtons slot="end">
-              <IonButton onClick={() => setIsOpen(false)}>Close</IonButton>
-            </IonButtons>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent className="ion-padding">
-          {/* {emojiObject} */}
+    </IonContent >
+
+  );
+};
+export default ExploreContainer;
+
+// Modal function
+const MyModal: React.FC<any> = ({ initialData, isOpen, onClose }) => {
+
+  const emoji = initialData
+
+
+  return (
+
+    <IonModal isOpen={isOpen}>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonButton onClick={onClose}>Close</IonButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent >
+
+        <div className="modal-container">
+
+          <IonImg src={emoji.file} alt={emoji.name} id="modalImg" />
+          {/* need to makes the h1 switch between arrernte(katj and english) */}
+          <h1> {emoji.name_arrernte}</h1>
+          <IonChip>Arrernte</IonChip>
+          <IonChip>English</IonChip>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni illum quidem recusandae ducimus quos
             reprehenderit. Veniam, molestias quos, dolorum consequuntur nisi deserunt omnis id illo sit cum qui.
             Eaque, dicta.
           </p>
+        </div>
+      </IonContent>
+    </IonModal >
 
-        </IonContent>
-      </IonModal>
-    </IonContent >
-
-  );
-};
-
-export default ExploreContainer;
+  )
+}
