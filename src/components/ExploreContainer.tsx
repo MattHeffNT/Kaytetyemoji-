01254030c86b40c5d26be5da6dc9c989349cdff2
MattHeffNT <heffa2004@gmail.com>
2022-09-13 01:16:51 +1000

    clean up comments, remove redundant code

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -3,8 +3,8 @@ import emojis from '../assets/emojis.json'
 import { useState } from 'react';
 
 // import Ion components (seperated into two lines for readability)
-import { IonPage, IonContent, IonSearchbar, IonHeader, IonToolbar, IonTitle, IonImg, IonGrid, IonRow, IonCol } from '@ionic/react';
-import { IonModal, IonChip, IonButtons, IonButton } from '@ionic/react';
+import { IonContent } from '@ionic/react';
+// import { IonModal, IonChip, IonButtons, IonButton } from '@ionic/react';
 // import MyModal from './MyModal'
 
 // import { useParams } from 'react-router';
@@ -19,11 +19,11 @@ interface ContainerProps {
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
 
-  const [emojisData, setEmojisData] = useState([]);
-  const [searchText, setSearchText] = useState('');
+  // const [emojisData, setEmojisData] = useState([]);
+  // const [searchText, setSearchText] = useState('');
 
   // maybe have another name setter here so i can call it on a close modal function
-  const [myModal, setMyModal] = useState({ isOpen: false })
+  // const [myModal, setMyModal] = useState({ isOpen: false })
 
   // get array of emoji json object keys before using map method
   const result: any = Object.keys(emojis).map((key) => {
