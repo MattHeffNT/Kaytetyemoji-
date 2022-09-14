cdd51aebc4409005c0934391c1b7745c86951e2c
MattHeffNT <heffa2004@gmail.com>
2022-09-14 10:57:38 +1000

    clean up comments, add component links

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -4,14 +4,13 @@ import { useState } from 'react';
 
 // import Ion components (seperated into two lines for readability)
 import { IonContent } from '@ionic/react';
-// import { IonModal, IonChip, IonButtons, IonButton } from '@ionic/react';
-// import MyModal from './MyModal'
 
 // import { useParams } from 'react-router';
 import { useLocation } from 'react-router-dom';
 import Info from './Info'
 import Home from './Home'
-
+import Faq from './Faq'
+import Team from './Team'
 
 interface ContainerProps {
   name: string;
@@ -19,20 +18,15 @@ interface ContainerProps {
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
 
-  // const [emojisData, setEmojisData] = useState([]);
-  // const [searchText, setSearchText] = useState('');
 
-  // maybe have another name setter here so i can call it on a close modal function
-  // const [myModal, setMyModal] = useState({ isOpen: false })
+  // // get array of emoji json object keys before using map method
+  // const result: any = Object.keys(emojis).map((key) => {
+  //   return { [key]: emojis[key as keyof typeof emojis] };
+  // });
 
-  // get array of emoji json object keys before using map method
-  const result: any = Object.keys(emojis).map((key) => {
-    return { [key]: emojis[key as keyof typeof emojis] };
-  });
-
-  // variables to map in the component
-  var obj: any = emojis
-  var arr: any = obj.emojis
+  // // variables to map in the component
+  // var obj: any = emojis
+  // var arr: any = obj.emojis
 
 
   const location = useLocation();
@@ -46,6 +40,8 @@ const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
       {/* render component based on rest */}
       {(name == "Home") && <Home />}
       {(name == "Information") && <Info />}
+      {(name == "Faq") && <Faq />}
+      {(name == "Team") && <Team />}
 
 
 
