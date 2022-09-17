ae328094dd1bb3bc731cf440c526035c00b7ab62
MattHeffNT <heffa2004@gmail.com>
2022-09-17 10:54:20 +1000

    fix pagination style

--- src/components/Slides.tsx
+++ src/components/Slides.tsx
@@ -3,6 +3,7 @@ import { IonModal, IonToolbar, IonButtons, IonSlides, IonSlide, IonContent, IonB
 import { IonGrid, IonCol, IonRow } from '@ionic/react';
 import IonIcon from '@reacticons/ionicons';
 import './Slides.css';
+import '../theme/variables.css'
 
 // Optional parameters to pass to the swiper instance.
 // See https://swiperjs.com/swiper-api for valid options.
@@ -15,10 +16,8 @@ const Slides: React.FC<any> = ({ isOpen, onClose }) => {
 
   const slideOpts = {
     // arrays start at 0 remember, so 0 is the first one lol 
-    autoHeight: 20,
-    // spaceBetween: 30,
     initialSlide: 0,
-    speed: 300,
+    speed: 400,
   };
   return (
 
@@ -60,44 +59,52 @@ const Slides: React.FC<any> = ({ isOpen, onClose }) => {
             </IonGrid>
           </IonSlide>
           <IonSlide>
-
-
-            <IonCol></IonCol>
-            <IonCol>
-              <div className="slide-container">
-                <IonImg src='../assets/screen1_header.png' id="drawing" />
-                <h1>Werte!</h1>
-
-                <strong><p>Anwerne akangentye arrantherre apetyeke Indigemoji-kenhe aretyeke. Arne-nhenhe-areye arrekantherre ileme anwerne-akerte, altyerre, arne, awelhentye-areye, angkentye anwernekenhe-uthene. Impene anthurre kwenhe arrantherre angkentye akaltye-irretyeke, angkentye anwernekenhe akwete-ante rlterrke antirrkwetyeke. </p></strong>
-                <p>Welcome to the Indiemoji app! This sticker set represents life, culture and language in our part of Central Australia, the traditional land of the Arrernte people. It is important you learn so that we hold our language strong, forever.</p>
-              </div>
-            </IonCol>
+            <IonGrid>
+              <IonRow>
+                <IonCol>
+                  <IonImg src='../assets/screen1_header.png' id="drawing" />
+                </IonCol>
+                <IonCol>
+                  <div className="slide-container">
+                    <h1>Werte!</h1>
+                    <strong><p>Anwerne akangentye arrantherre apetyeke Indigemoji-kenhe aretyeke. Arne-nhenhe-areye arrekantherre ileme anwerne-akerte, altyerre, arne, awelhentye-areye, angkentye anwernekenhe-uthene. Impene anthurre kwenhe arrantherre angkentye akaltye-irretyeke, angkentye anwernekenhe akwete-ante rlterrke antirrkwetyeke. </p></strong>
+                    <p>Welcome to the Indiemoji app! This sticker set represents life, culture and language in our part of Central Australia, the traditional land of the Arrernte people. It is important you learn so that we hold our language strong, forever.</p>
+                  </div>
+                </IonCol>
+              </IonRow>
+            </IonGrid>
           </IonSlide>
           <IonSlide>
-          </IonRow>
-          <div className="slide-container">
-            <IonImg src='../assets/screen2_header.png' id="drawing" />
-            <strong><p>Tyerretye anwerkenhe-arle itnhenhe-areye mpwareke. Arrernte ilyernpenye-areye help-me-ileme angkentye arratye arrerneke.</p></strong>
-            <p>These emoji stickers were made by hundreds of young people with senior Arrernte cultural advisers, for you to use.</p>
-          </div>
-        </IonRow>
-      </IonSlide>
-      <IonSlide>
-        <div className="slide-container">
-          <IonImg src='../assets/screen3_header.png' id="drawing" />
-          <h1>Antherreme!</h1>
-          <strong><p>Unte apeke emoji renhe yernetyeke, app nhenhe altywere-ilaye, kele emoji unte ahentye-aneme uthemele, kele imernte antherreme-kenhe-button ulthemele.</p></strong>
-          <p>To share an emoji sticker, just open this app, select the sticker you want to use and then tap the share button. You can then choose which platform you want to share the sticker on.</p>
-        </div>
-      </IonSlide>
-      <IonSlide>
-        <div className="slide-container">
-          <IonImg src='../assets/screen5_header.png' id="drawing" />
-          <h1>Awaye!</h1>
-          <p>New audio! Press the play button to learn to pronounce the Arrernte emojis</p>
-        </div>
-      </IonSlide>
-    </IonSlides>
+            <IonGrid>
+              <IonRow>
+                <IonCol>
+                  <IonImg src='../assets/screen2_header.png' id="drawing" />
+                </IonCol>
+                <IonCol>
+                  <div className="slide-container">
+                    <strong><p>Tyerretye anwerkenhe-arle itnhenhe-areye mpwareke. Arrernte ilyernpenye-areye help-me-ileme angkentye arratye arrerneke.</p></strong>
+                    <p>These emoji stickers were made by hundreds of young people with senior Arrernte cultural advisers, for you to use.</p>
+                  </div>
+                </IonCol>
+              </IonRow>
+            </IonGrid>
+          </IonSlide>
+          <IonSlide>
+            <div className="slide-container">
+              <IonImg src='../assets/screen3_header.png' id="drawing" />
+              <h1>Antherreme!</h1>
+              <strong><p>Unte apeke emoji renhe yernetyeke, app nhenhe altywere-ilaye, kele emoji unte ahentye-aneme uthemele, kele imernte antherreme-kenhe-button ulthemele.</p></strong>
+              <p>To share an emoji sticker, just open this app, select the sticker you want to use and then tap the share button. You can then choose which platform you want to share the sticker on.</p>
+            </div>
+          </IonSlide>
+          <IonSlide>
+            <div className="slide-container">
+              <IonImg src='../assets/screen5_header.png' id="drawing" />
+              <h1>Awaye!</h1>
+              <p>New audio! Press the play button to learn to pronounce the Arrernte emojis</p>
+            </div>
+          </IonSlide>
+        </IonSlides>
       </IonContent >
     </IonModal >
   )
