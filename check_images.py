8daf4c194cce2ee58693c120e592d63e1ab8a386
MattHeffNT <heffa2004@gmail.com>
2022-09-24 17:44:51 +1000

    update json script with latest one by Sam

---
+++ check_images.py
@@ -0,0 +1,33 @@
+import glob
+import json
+import os
+import csv
+import base64
+
+ios_path = "ios/indigemoji/Images.xcassets/{}.imageset/{}.png"
+ios_sticker_path = "ios/IndigemojiStickers/Stickers.xcassets/Sticker Pack.stickerpack/{}.sticker/{}.png"
+
+out = {"emojis": []}
+
+with open("./emojis.csv", "r") as infile:
+    reader = csv.DictReader(infile)
+    for row in reader:
+        with open("./src/assets/emojis/" + row["file"], "rb") as imagefile:
+            imgdata = base64.b64encode(imagefile.read()).decode("utf8")
+        item = {
+            "name": row["name"],
+            "name_arrernte": row["name_arrernte"],
+            "file": row["file"],
+            "data": imgdata,
+        }
+
+        if row["audio"] != "" and not os.path.isfile(
+            "./src/assets/audio/%s" % row["audio"]
+        ):
+            print("missing %s" % row["audio"])
+        else:
+            item["audio"] = row["audio"]
+        out["emojis"].append(item)
+
+with open("./src/assets/emojis.json", "w") as outfile:
+    json.dump(out, outfile, indent=2)
