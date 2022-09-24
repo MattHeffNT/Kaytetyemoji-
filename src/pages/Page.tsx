75bf86675180f1268193d9d0353c672e10c0928d
MattHeffNT <heffa2004@gmail.com>
2022-09-25 01:16:22 +1000

    better formatting, resized/change logo

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,42 +1,51 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
 import { useParams } from 'react-router';
 import ExploreContainer from '../components/ExploreContainer';
 import './Page.css';
-import homeIcon from '../assets/icon.png'
-import background from '../assets/images/background.png'
+import homeIcon from '../assets/images/start_header_no.png';
+import background from '../assets/images/background.png';
+import Home from '../components/Home';
 
+import { Redirect, Route } from 'react-router-dom';
 
 const Page: React.FC = () => {
-
   // this grabs the link name/title and puts it as  the page title
-  const { name } = useParams<{ name: string; }>();
+  const { name } = useParams<{ name: string }>();
 
   return (
     <IonPage>
       <IonHeader id="header">
         <IonToolbar>
           <IonButtons slot="end">
+            {/* side menu hamburger button  */}
             <IonMenuButton />
           </IonButtons>
-
           {/* put the indigemoji icon on the toolbar */}
-          <IonTitle><a href='/'><img src={homeIcon} id="homeIcon" alt="icon"></img></a></IonTitle>
-
+          <IonTitle>
+            <img src={homeIcon} id="homeIcon" alt="icon"></img>
+          </IonTitle>
         </IonToolbar>
       </IonHeader>
 
       <IonContent fullscreen>
         <IonHeader collapse="condense">
-          <IonToolbar>
-
-          </IonToolbar>
+          <IonToolbar></IonToolbar>
         </IonHeader>
 
-        {/* Remove name prop from being passed each page, also place search here maybe 
-        if want to seperate states from container  */}
+        {/* component could make this a conditonal...so depending on what url input is called, then call a different 
+        component? */}
+
         <ExploreContainer name={name} />
       </IonContent>
-    </IonPage >
+    </IonPage>
   );
 };
 
