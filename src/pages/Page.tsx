4223e43e385c598c4dae9f9b07e1e80aa1f31b34
MattHeffNT <heffa2004@gmail.com>
2022-10-06 17:39:46 +1100

    IndigEmoji logo links to home now

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,52 +1,63 @@
 import {
-  IonButtons,
-  IonContent,
-  IonHeader,
-  IonMenuButton,
-  IonPage,
-  IonTitle,
-  IonToolbar,
+    IonButtons,
+    IonButton,
+    // IonItem,
+    IonContent,
+    IonHeader,
+    IonMenuButton,
+    IonPage,
+    IonTitle,
+    IonImg,
+    IonToolbar,
+    // IonRouterLink,
 } from '@ionic/react';
 import { useParams } from 'react-router';
 import ExploreContainer from '../components/ExploreContainer';
 import './Page.css';
 import homeIcon from '../assets/images/start_header_no.png';
-import background from '../assets/images/background.png';
-import Home from '../components/Home';
+// import background from '../assets/images/background.png';
+// import Home from '../components/Home';
+// import { useState } from 'react';
 
-import { Redirect, Route } from 'react-router-dom';
+// import { Redirect, Route } from 'react-router-dom';
 
 const Page: React.FC = () => {
-  // this grabs the link name/title and puts it as  the page title
-  const { name } = useParams<{ name: string }>();
+    // this grabs the link name/title and puts it as  the page title
+    var { name } = useParams<{ name: string }>();
 
-  return (
-    <IonPage>
-      <IonHeader id="header">
-        <IonToolbar>
-          <IonButtons slot="end">
-            {/* side menu hamburger button  */}
-            <IonMenuButton />
-          </IonButtons>
-          {/* put the indigemoji icon on the toolbar */}
-          <IonTitle>
-            <img src={homeIcon} id="homeIcon" alt="icon"></img>
-          </IonTitle>
-        </IonToolbar>
-      </IonHeader>
+    return (
+        <IonPage>
+            <IonHeader id="header">
+                <IonToolbar>
+                    <IonButtons slot="end">
+                        {/* side menu hamburger button  */}
+                        <IonMenuButton />
+                    </IonButtons>
+                    <IonButtons slot="start">
+                        {/* side menu hamburger button  */}
+                        <IonButton
+                            routerLink="/page/Home"
+                            routerDirection="none"
+                            id="home-icon-button"
+                        >
+                            <IonImg src={homeIcon} id="homeIcon" />
+                        </IonButton>
+                    </IonButtons>
+                </IonToolbar>
+            </IonHeader>
 
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar></IonToolbar>
-        </IonHeader>
+            <IonContent fullscreen>
+                <IonHeader collapse="condense">
+                    <IonToolbar></IonToolbar>
+                </IonHeader>
 
-        {/* component could make this a conditonal...so depending on what url input is called, then call a different 
+                {/* component could make this a conditonal...so depending on what url input is called, then call a different 
         component? */}
 
-        <ExploreContainer name={name} />
-      </IonContent>
-    </IonPage>
-  );
+                <ExploreContainer name={name} />
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Page;
