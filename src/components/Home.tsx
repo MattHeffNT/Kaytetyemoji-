d66379703d7c048bbeb5f98d38d1e4ae8202dd65
MattHeffNT <heffa2004@gmail.com>
2023-01-14 15:09:37 +1100

    add slide on first load

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,101 +3,135 @@ import emojis from '../assets/emojis.json';
 import { useState, useEffect, useRef, useMemo, useCallback, ChangeEvent } from 'react';
 // import { useLocation } from 'react-router';
 // import Ion components (seperated into two lines for readability)
-import { IonContent, IonSearchbar, IonImg, IonGrid, IonRow, IonCol } from '@ionic/react';
+import {
+    IonLoading,
+    useIonViewWillEnter,
+    IonContent,
+    IonSearchbar,
+    IonImg,
+    IonGrid,
+    IonRow,
+    IonCol,
+} from '@ionic/react';
 import MyModal from './MyModal';
+import Slides from './Slides';
 
 const Home: React.FC = () => {
-  const [emojisData, setEmojisData] = useState([]);
-  const [searchText, setSearchText] = useState('');
-  const [myModal, setMyModal] = useState({ isOpen: false });
+    const [emojisData, setEmojisData] = useState([]);
+    const [searchText, setSearchText] = useState('');
+    const [myModal, setMyModal] = useState({ isOpen: false });
 
-  // variables to map in the component
-  var obj: any = emojis;
-  var arr: any = obj.emojis;
-  const searchBar: any = useRef();
-  const emojiColumn: any = useRef();
-  const emojiArray: any | undefined = useRef();
+    const [isOpen, setIsOpen] = useState(false);
+    // variables to map in the component
+    var obj: any = emojis;
+    var arr: any = obj.emojis;
+    const searchBar: any = useRef();
+    const emojiColumn: any = useRef();
+    const emojiArray: any | undefined = useRef();
 
-  // search bar
-  useEffect(() => {
-    const rows = Array.from(emojiArray.current.children);
+    const imageHandler = () => {
+        console.log('finished loading images');
+    };
 
-    // listen for search bar input then call function
-    searchBar.current.addEventListener('ionInput', handleInput);
+    // show welcome slides if the user hasn't opened the app before
+    useIonViewWillEnter(() => {
+        const openedBefore = localStorage.getItem('openedBefore');
+        if (!openedBefore || openedBefore == null) {
+            setIsOpen(true);
+            localStorage.setItem('openedBefore', 'true');
+        }
+    });
 
-    function handleInput(event: React.MouseEvent<HTMLInputElement, ChangeEvent>) {
-      const query = (event.target as HTMLInputElement).value.toLowerCase();
+    // search bar
+    useEffect(() => {
+        const rows = Array.from(emojiArray.current.children);
+        // listen for search bar input then call function
+        searchBar.current.addEventListener('ionInput', handleInput);
 
-      requestAnimationFrame(() => {
-        rows.forEach((col: any) => {
-          const emoji = col.firstElementChild;
+        function handleInput(event: React.MouseEvent<HTMLInputElement, ChangeEvent>) {
+            const query = (event.target as HTMLInputElement).value.toLowerCase();
 
-          const shouldShow = emoji.id.toLowerCase().indexOf(query) > -1;
+            requestAnimationFrame(() => {
+                rows.forEach((col: any) => {
+                    const emoji = col.firstElementChild;
 
-          // change container and emoji display status depending on whether input detected
-          if (shouldShow) {
-            // reset gridded emojis to default style
-            col.style = 'display:flex';
-            emoji.style = 'display:inherit;';
-            col.style =
-              'flex: 0 0 calc(calc(3 / var(--ion-grid-columns, 12)) * 100%); width: calc(calc(3 / var(--ion - grid - columns, 12)) * 100 %); max - width: calc(calc(3 / var(--ion - grid - columns, 12)) * 100 %);';
-          } else {
-            // don't display other emoji's when searching
-            col.style =
-              'flex: 0 0 calc(calc(3 / var(--ion-grid-columns, 12)) * 100%); width: calc(calc(3 / var(--ion - grid - columns, 12)) * 100 %); max - width: calc(calc(3 / var(--ion - grid - columns, 12)) * 100 %);';
-            emoji.style = 'display:none;';
-            col.style = 'display:none';
-          }
-        });
-      });
-    }
-  }, []);
+                    const shouldShow = emoji.id.toLowerCase().indexOf(query) > -1;
 
-  // map the emojis to the homescreen and pass values onto modal on click
-  const emojiElements = useCallback(
-    useMemo(() => {
-      return arr.map((emoji: any, index: number) => (
-        <IonCol size="3" ref={emojiColumn} key={index}>
-          <IonImg
-            src={emoji.file}
-            id={emoji.name}
-            className="emoji"
-            onClick={() => {
-              setMyModal({ isOpen: true });
-              setEmojisData(emoji);
-            }}
-          />
+                    // change container and emoji display status depending on whether input detected
+                    if (shouldShow) {
+                        // reset gridded emojis to default style
+                        col.style = 'display:flex';
+                        emoji.style = 'display:inherit;';
+                        col.style =
+                            'flex: 0 0 calc(calc(3 / var(--ion-grid-columns, 12)) * 100%); width: calc(calc(3 / var(--ion - grid - columns, 12)) * 100 %); max - width: calc(calc(3 / var(--ion - grid - columns, 12)) * 100 %);';
+                    } else {
+                        // don't display other emoji's when searching
+                        col.style =
+                            'flex: 0 0 calc(calc(3 / var(--ion-grid-columns, 12)) * 100%); width: calc(calc(3 / var(--ion - grid - columns, 12)) * 100 %); max - width: calc(calc(3 / var(--ion - grid - columns, 12)) * 100 %);';
+                        emoji.style = 'display:none;';
+                        col.style = 'display:none';
+                    }
+                });
+            });
+        }
+    }, []);
+
+    // map the emojis to the homescreen and pass values onto modal on click
+    // maybe i can add a function here to double check whether the emojis have loaded in or not
+    // then to just load the ones already there, and or render if they haven't
+
+    const emojiElements = arr.map((emoji: any, index: number) => (
+        <IonCol size="3" key={index} ref={emojiColumn}>
+            <IonImg
+                key={index}
+                src={emoji.file}
+                id={emoji.name}
+                className="emoji"
+                onClick={() => {
+                    setMyModal({ isOpen: true });
+                    setEmojisData(emoji);
+                }}
+                onIonImgDidLoad={() => {
+                    imageHandler();
+                }}
+            />
         </IonCol>
-      ));
-    }, [arr, emojiColumn]),
-    []
-  );
+    ));
 
-  return (
-    <IonContent>
-      <div className="container">
-        {/* search */}
-        <IonSearchbar
-          ref={searchBar}
-          id="search"
-          value={searchText}
-          onIonChange={(e) => setSearchText(e.detail.value!)}
-          searchIcon="none"
-          showClearButton="always"
-        ></IonSearchbar>
-        <IonGrid>
-          <IonRow ref={emojiArray}>{emojiElements}</IonRow>
-        </IonGrid>
-      </div>
-      <MyModal
-        isOpen={myModal.isOpen}
-        onClose={(e: Event) => {
-          setMyModal({ isOpen: false });
-          return e;
-        }}
-        initialData={emojisData}
-      />
-    </IonContent>
-  );
+    return (
+        <IonContent>
+            {/* welcome slides */}
+            <Slides
+                isOpen={isOpen}
+                onClose={() => {
+                    setIsOpen(false);
+                }}
+            />
+
+            <div className="container">
+                {/* search */}
+                <IonSearchbar
+                    ref={searchBar}
+                    id="search"
+                    value={searchText}
+                    onIonChange={(e) => setSearchText(e.detail.value!)}
+                    searchIcon="none"
+                    showClearButton="always"
+                ></IonSearchbar>
+                {/* map the emojis  */}
+                <IonGrid>
+                    <IonRow ref={emojiArray}>{emojiElements}</IonRow>
+                </IonGrid>
+            </div>
+            <MyModal
+                isOpen={myModal.isOpen}
+                onClose={(e: Event) => {
+                    setMyModal({ isOpen: false });
+                    return e;
+                }}
+                initialData={emojisData}
+            />
+        </IonContent>
+    );
 };
 export default Home;
