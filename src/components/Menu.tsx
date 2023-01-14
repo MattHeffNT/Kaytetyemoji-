de403ffc4dcbf757238baa20174a134413cc7c56
MattHeffNT <heffa2004@gmail.com>
2023-01-14 15:11:19 +1100

    prep adding dark mode toggle in side menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,20 +1,13 @@
-import {
-    IonContent,
-    IonItem,
-    IonList,
-    IonMenu,
-    IonMenuToggle,
-    IonImg,
-} from '@ionic/react';
+import { IonContent, IonItem, IonList, IonMenu, IonMenuToggle, IonImg } from '@ionic/react';
 
 import homeIcon from '../assets/images/homeIcon.png';
 import exclaimIcon from '../assets/images/infoIcon.png';
 import questionIcon from '../assets/images/icon4.png';
 import peopleIcon from '../assets/images/icon5.png';
-
 import { useLocation } from 'react-router-dom';
 import './styles/Menu.css';
 
+import IonIcon from '@reacticons/ionicons';
 interface AppPage {
     url: string;
     title: string;
@@ -42,18 +35,50 @@ const appPages: AppPage[] = [
         url: '/page/Team',
         image: peopleIcon,
     },
+    // add darkmode switch, icon changes on press betweeen moon/sun
+    // {
+    //     title: 'darkMode',
+    //     // link to onclick function
+    //     url: '/page/Team',
+    //     image: `<IonIcon name="moon-outline"/>`,
+    // },
 ];
 
 const Menu: React.FC = () => {
     const location = useLocation();
 
+    function darkMode() {
+        // Use matchMedia to check the user preference
+        const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+        // Query for the toggle that is used to change between themes
+        const toggle = document.querySelector('#themeToggle');
+
+        // Listen for the toggle check/uncheck to toggle the dark class on the <body>
+        //@ts-ignore
+        toggle.addEventListener('ionChange', (ev) => {
+            // document.body.classList.toggle('dark', ev.detail.checked);
+            console.log(ev);
+        });
+
+        // const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+        // Listen for changes to the prefers-color-scheme media query
+        // prefersDark.addListener((e) => checkToggle(e.matches));
+
+        // Called when the app loads
+        // function loadApp() {
+        //     checkToggle(prefersDark.matches);
+        // }
+
+        // Called by the media query to check/uncheck the toggle
+        // function checkToggle(shouldCheck) {
+        //     toggle.checked = shouldCheck;
+        // }
+    }
+
     return (
-        <IonMenu
-            contentId="main"
-            type="overlay"
-            side="end"
-            className="my-custom-menu"
-        >
+        <IonMenu contentId="main" type="overlay" side="end" className="my-custom-menu">
             <IonContent>
                 {/* </IonList> */}
                 <IonList id="sideMenu">
@@ -61,11 +86,7 @@ const Menu: React.FC = () => {
                         return (
                             <IonMenuToggle key={index} autoHide={true}>
                                 <IonItem
-                                    className={
-                                        location.pathname === appPage.url
-                                            ? 'selected'
-                                            : ''
-                                    }
+                                    className={location.pathname === appPage.url ? 'selected' : ''}
                                     routerLink={appPage.url}
                                     routerDirection="none"
                                     lines="none"
@@ -77,6 +98,14 @@ const Menu: React.FC = () => {
                             </IonMenuToggle>
                         );
                     })}
+                    {/* should probably just keep this as it's normal color as the other menu icons are */}
+                    {/* actually dark and will probably need a light version */}
+                    {/* <IonIcon */}
+                    {/*     name="moon-outline" */}
+                    {/*     className="modal-icon" */}
+                    {/*     size="large" */}
+                    {/*     onClick={darkMode} */}
+                    {/* /> */}
                 </IonList>
             </IonContent>
         </IonMenu>
