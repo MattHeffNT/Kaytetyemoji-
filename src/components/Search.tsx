f86e5a02d68892d46c492d0767467bdc46da01da
MattHeffNT <heffa2004@gmail.com>
2023-03-26 12:33:28 +1100

    search component  created to clean up home module

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -7,41 +7,47 @@ interface SearchProps {
 }
 
 const Search: React.FC<SearchProps> = ({ setSearchText, emojiArray }) => {
-    const searchBar: any = useRef();
-    useEffect(() => {
-        // null check
-        if (searchBar.current) {
-            // listen for search bar input then call function
-            searchBar.current.addEventListener('ionInput', handleInput);
-        }
-        // when user starts typing into the searchbar handle the change event
-        function handleInput(event: React.MouseEvent<HTMLInputElement, ChangeEvent>) {
-            const query = (event.target as HTMLInputElement).value.toLowerCase();
-            const rows = Array.from(emojiArray.current.children);
-            setSearchText(query);
-            requestAnimationFrame(() => {
-                rows.forEach((col: any) => {
-                    const emoji = col.firstElementChild;
-                    const shouldShow = emoji.id.toLowerCase().indexOf(query) > -1;
+    const searchBarRef = useRef<HTMLIonSearchbarElement>(null);
 
-                    // change container and emoji display status depending on whether search input detected
-                    if (shouldShow) {
-                        // reset gridded emojis to default style
-                        col.classList.remove('resetGrid');
-                    } else {
-                        col.classList.add('resetGrid');
-                    }
-                });
+    function handleInput(event: React.MouseEvent<HTMLInputElement, ChangeEvent>) {
+        const query = (event.target as HTMLInputElement).value.toLowerCase();
+        const rows = Array.from(emojiArray.current.children);
+        setSearchText(query);
+        requestAnimationFrame(() => {
+            rows.forEach((col: any) => {
+                const emoji = col.firstElementChild;
+                const shouldShow = emoji.id.toLowerCase().indexOf(query) > -1;
+
+                // change container and emoji display status depending on whether search input detected
+                if (shouldShow) {
+                    // reset gridded emojis to default style
+                    col.classList.remove('resetGrid');
+                } else {
+                    col.classList.add('resetGrid');
+                }
             });
+        });
+    }
+
+    useEffect(() => {
+        const searchBar = searchBarRef.current;
+        // null check
+        if (searchBar) {
+            // listen for search bar input then call function
+            searchBar.addEventListener('ionInput', handleInput as any);
         }
+
         return () => {
-            searchBar.current.removeEventListener('ionInput', handleInput);
+            if (searchBar) {
+                searchBar.removeEventListener('ionInput', handleInput as any);
+            }
         };
-    }, [emojiArray, setSearchText]);
+    }, [searchBarRef, handleInput]);
 
     return (
         <IonSearchbar
-            ref={searchBar}
+            style={{ fontFamily: 'open sans' }}
+            ref={searchBarRef}
             onIonChange={(e) => setSearchText(e.detail.value!)}
             placeholder="Search"
         />
