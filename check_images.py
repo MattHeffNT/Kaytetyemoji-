a417e69904db5f95387c84bec3d3e0c667de1db7
MattHeffNT <heffa2004@gmail.com>
2023-03-26 11:38:14 +1100

    updated a bit to reflect new object properties

--- check_images.py
+++ check_images.py
@@ -1,13 +1,13 @@
 """
 Script by Sam Lavigne aka Antiboredom https://github.com/antiboredom
-that uses the csv file to generate the paths to the emojis and audio in a json file.
-as well as converts the image to a base64 uri.
+that uses the csv file to generate the paths to the emojis and audio
+in a json file. As well as converts the image to a base64 uri.
 
 """
 
 import base64
 import csv
-import glob
+# import glob
 import json
 import os
 
@@ -25,6 +25,14 @@ with open("./emojis.csv", "r") as infile:
         if not row['audio']:
             # skip empty lines
             continue
+        if not row['phrases']:
+            # skip empty lines
+            row['phrases'] = ""
+        #     continue
+        # if not row['phrases_kaytetye']:
+        #     continue
+        # if not row['phrases_english']:
+        #     continue
         with open("./src/assets/emojis/" + row["file"], "rb") as imagefile:
             imgdata = base64.b64encode(imagefile.read()).decode("utf8")
 
@@ -34,16 +42,19 @@ with open("./emojis.csv", "r") as infile:
             "name_kaytetye": row["name_kaytetye"],
             "file": "../assets/emojis/" + row["file"],
             "data": imgdata,
-            "audio": "../assets/audio/" + row["audio"]
+            "audio": "../assets/audio/" + row["audio"],
+            "phrases": "../assets/phrases/" + row["phrases"],
+            "phrases_kaytetye": row["phrases_kaytetye"],
+            "phrases_english": row["phrases_english"]
         }
         if row["audio"] != "" and not os.path.exists(
             "../assets/audio/%s" % row["audio"]
         ):
-
             print("missing %s" % row["audio"])
         else:
             item["audio"] = "./src/assets/audio/" + row["audio"]
         out["emojis"].append(item)
+
 
 with open("./src/assets/emojis.json", "w") as outfile:
     json.dump(out, outfile, indent=2)
