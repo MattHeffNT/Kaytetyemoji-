5bbead9b174120ac06e4a97fa29d795e47677344
MattHeffNT <heffa2004@gmail.com>
2023-03-26 12:32:31 +1100

    text updated with katij information

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -4,63 +4,63 @@ import { useState } from 'react';
 import { IonContent } from '@ionic/react';
 
 const Info: React.FC<any> = () => {
-  const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <IonContent>
-      <div className="info-container">
-        <h1> Emoji-akerte / About </h1>
-        <strong>
-          <p>
-            Emoji itne-areye Australia mpwepe-arenye. itne ayeye anwerne-akerte, altyerre-akerte,
-            angkentye akerte uthene. Arrernte angkentye anwernekenhe impene anthurre, altyerrenge.
-            Australian-mpwepe angkentye atningke akerte kenhe Arrernte Mparntwe-arenye kwenhe.
-            Marle, urreye, arelhe, artwe-areye-uthene-arle emoji-kenhe arlkenye intelheleke
-            uterne-ureke. Emoji arrpe-anenhe arritnye Arrernte-kenhe-akerte, anwerne ahentye
-            arrantherre itnenhe akaltye-irretyeke. Anwerne internet-nge itnenhe arrerneke kele
-            angkentye anwernekenhe rlterrke-aneme-akwete.
-          </p>
-        </strong>
-        <p>
-          This emoji sticker set represents life, culture and language on Arrernte Country in
-          Central Australia. Each design was developed during seven weeks of workshops held at the
-          Alice Springs Public Library over the summer school holidays of 2018 as part of the Geek
-          in Residence program. These digital art workshops ran all day, seven days a week, with
-          over 1000 participants from Mparntwe/Alice Springs as well as from many remote
-          communities. The young designers were supported by a team of senior Indigenous artists,
-          Digital Mentors and Arrernte linguists to help them realise their ideas.
-        </p>
-        <p>
-          Each emoji has an Arrernte name, the ancient language of our place, that we hope you’ll
-          learn. It’s just one way of sharing our culture online and contributing to the
-          decolonisation of the internet for our young people. We hope we can use the internet to
-          keep our language strong.
-        </p>
-        {/* onClick launch modal/slide component? */}
-        <p
-          onClick={() => {
-            setIsOpen(true);
-          }}
-          id="slide-link"
-        >
-          View the welcome screens.
-        </p>
-        <p> Read more about the emojis on the </p>
-        <a href="https://www.indigemoji.com.au/">Indigemoji website</a>.
-        <br />
-        <p>
-          <a href="https://www.indigemoji.com.au/terms-conditions">
-            View terms and conditions of use.
-          </a>
-        </p>
-        <Slides
-          isOpen={isOpen}
-          onClose={() => {
-            setIsOpen(false);
-          }}
-        />
-      </div>
-    </IonContent>
-  );
+    return (
+        <IonContent>
+            <div className="info-container">
+                <h1> Emoji-we // About </h1>
+                <p>
+                    After the success of the Indigemoji app, additional resources became available
+                    to invest in innovative language projects. The Arrernte Indigemoji team
+                    approached linguist Dr Myfany Turpin to propose assisting the Kaytetye community
+                    to develop a project, as they knew Kaytetye was considered a highly endangered
+                    Australian language, with only 109 speakers listed in the 2021 census, which was
+                    down nine per cent since the previous census.
+                </p>
+                <p>
+                    Over many months, our team went through a process of translating the relevant
+                    Indigemojis from Arrernte to Kaytetye as well as designing 44 new emojis for
+                    plants, animals and other important parts of Kaytetye life and culture, with the
+                    help of graphic designers. We made recordings of the word to match each emoji,
+                    along with example phrases which you can find on this app.
+                </p>
+                <p>
+                    The main emoji representing our set is ‘artnke’ which means flat-topped hills, a
+                    significant feature of Kaytetye country; and the name of the hills that you see
+                    when you come into Barrow Creek. Other neighbouring language groups also
+                    recognise artnke as a feature of Kaytetye country.
+                </p>
+                <p>
+                    This is an app to encourage young people to learn Kaytetye. The digital
+                    generation never go anywhere without their phones and now they can share
+                    Kaytetye!
+                </p>
+                {/* onClick launch modal/slide component? */}
+                <p
+                    onClick={() => {
+                        setIsOpen(true);
+                    }}
+                    id="slide-link"
+                >
+                    View the welcome screens.
+                </p>
+                <p> Read more about the emojis on the </p>
+                <a href="https://www.indigemoji.com.au/">Artnke website</a>.
+                <br />
+                <p>
+                    <a href="https://www.indigemoji.com.au/terms-conditions">
+                        View terms and conditions of use.
+                    </a>
+                </p>
+                <Slides
+                    isOpen={isOpen}
+                    onClose={() => {
+                        setIsOpen(false);
+                    }}
+                />
+            </div>
+        </IonContent>
+    );
 };
 export default Info;
