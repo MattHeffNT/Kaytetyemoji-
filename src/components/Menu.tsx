e535d567ec468e836e48f6cff4fe489911d80f39
MattHeffNT <heffa2004@gmail.com>
2023-03-29 15:46:49 +1100

    remove dark mode

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -7,7 +7,6 @@ import peopleIcon from '../assets/images/icon5.png';
 import { useLocation } from 'react-router-dom';
 import './styles/Menu.css';
 
-import IonIcon from '@reacticons/ionicons';
 interface AppPage {
     url: string;
     title: string;
@@ -35,47 +34,10 @@ const appPages: AppPage[] = [
         url: '/page/Team',
         image: peopleIcon,
     },
-    // add darkmode switch, icon changes on press betweeen moon/sun
-    // {
-    //     title: 'darkMode',
-    //     // link to onclick function
-    //     url: '/page/Team',
-    //     image: `<IonIcon name="moon-outline"/>`,
-    // },
 ];
 
 const Menu: React.FC = () => {
     const location = useLocation();
-
-    function darkMode() {
-        // Use matchMedia to check the user preference
-        const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
-
-        // Query for the toggle that is used to change between themes
-        const toggle = document.querySelector('#themeToggle');
-
-        // Listen for the toggle check/uncheck to toggle the dark class on the <body>
-        //@ts-ignore
-        toggle.addEventListener('ionChange', (ev) => {
-            // document.body.classList.toggle('dark', ev.detail.checked);
-            console.log(ev);
-        });
-
-        // const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
-
-        // Listen for changes to the prefers-color-scheme media query
-        // prefersDark.addListener((e) => checkToggle(e.matches));
-
-        // Called when the app loads
-        // function loadApp() {
-        //     checkToggle(prefersDark.matches);
-        // }
-
-        // Called by the media query to check/uncheck the toggle
-        // function checkToggle(shouldCheck) {
-        //     toggle.checked = shouldCheck;
-        // }
-    }
 
     return (
         <IonMenu contentId="main" type="overlay" side="end" className="my-custom-menu">
@@ -100,12 +62,6 @@ const Menu: React.FC = () => {
                     })}
                     {/* should probably just keep this as it's normal color as the other menu icons are */}
                     {/* actually dark and will probably need a light version */}
-                    {/* <IonIcon */}
-                    {/*     name="moon-outline" */}
-                    {/*     className="modal-icon" */}
-                    {/*     size="large" */}
-                    {/*     onClick={darkMode} */}
-                    {/* /> */}
                 </IonList>
             </IonContent>
         </IonMenu>
