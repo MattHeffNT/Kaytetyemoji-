e5e049c4937e9abbadf61a49aefc4ccb5b5dbe40
MattHeffNT <heffa2004@gmail.com>
2023-04-06 00:40:12 +1000

    reregistered service worker, remove webvital reporting

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,10 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
@@ -20,4 +20,4 @@ serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
