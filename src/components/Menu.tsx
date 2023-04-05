74f8a0a734db60f62a11b3ff3b05a72b7fc11caf
MattHeffNT <heffa2004@gmail.com>
2023-04-05 18:22:45 +1000

    change to img not Ionimg

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -8,64 +8,64 @@ import { useLocation } from 'react-router-dom';
 import './styles/Menu.css';
 
 interface AppPage {
-    url: string;
-    title: string;
-    image: string;
+  url: string;
+  title: string;
+  image: string;
 }
 
 const appPages: AppPage[] = [
-    {
-        title: 'Home',
-        url: '/page/Home',
-        image: homeIcon,
-    },
-    {
-        title: 'Information',
-        url: '/page/Information',
-        image: exclaimIcon,
-    },
-    {
-        title: 'FAQ',
-        url: '/page/Faq',
-        image: questionIcon,
-    },
-    {
-        title: 'Team',
-        url: '/page/Team',
-        image: peopleIcon,
-    },
+  {
+    title: 'Home',
+    url: '/page/Home',
+    image: homeIcon,
+  },
+  {
+    title: 'Information',
+    url: '/page/Information',
+    image: exclaimIcon,
+  },
+  {
+    title: 'FAQ',
+    url: '/page/Faq',
+    image: questionIcon,
+  },
+  {
+    title: 'Team',
+    url: '/page/Team',
+    image: peopleIcon,
+  },
 ];
 
 const Menu: React.FC = () => {
-    const location = useLocation();
+  const location = useLocation();
 
-    return (
-        <IonMenu contentId="main" type="overlay" side="end" className="my-custom-menu">
-            <IonContent>
-                {/* </IonList> */}
-                <IonList id="sideMenu">
-                    {appPages.map((appPage, index) => {
-                        return (
-                            <IonMenuToggle key={index} autoHide={true}>
-                                <IonItem
-                                    className={location.pathname === appPage.url ? 'selected' : ''}
-                                    routerLink={appPage.url}
-                                    routerDirection="none"
-                                    lines="none"
-                                    detail={false}
-                                >
-                                    {/* <IonIcon slot="start" ios={appPage.image} md={appPage.image} /> */}
-                                    <IonImg src={appPage.image}></IonImg>
-                                </IonItem>
-                            </IonMenuToggle>
-                        );
-                    })}
-                    {/* should probably just keep this as it's normal color as the other menu icons are */}
-                    {/* actually dark and will probably need a light version */}
-                </IonList>
-            </IonContent>
-        </IonMenu>
-    );
+  return (
+    <IonMenu contentId="main" type="overlay" side="end" className="my-custom-menu">
+      <IonContent>
+        {/* </IonList> */}
+        <IonList id="sideMenu">
+          {appPages.map((appPage, index) => {
+            return (
+              <IonMenuToggle key={index} autoHide={true}>
+                <IonItem
+                  className={location.pathname === appPage.url ? 'selected' : ''}
+                  routerLink={appPage.url}
+                  routerDirection="none"
+                  lines="none"
+                  detail={false}
+                >
+                  {/* <IonIcon slot="start" ios={appPage.image} md={appPage.image} /> */}
+                  <img src={appPage.image}></img>
+                </IonItem>
+              </IonMenuToggle>
+            );
+          })}
+          {/* should probably just keep this as it's normal color as the other menu icons are */}
+          {/* actually dark and will probably need a light version */}
+        </IonList>
+      </IonContent>
+    </IonMenu>
+  );
 };
 
 export default Menu;
